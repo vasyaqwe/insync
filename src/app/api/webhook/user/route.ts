f446d9f3937ce68c465b63b8064982c3f398e5e3
vasyaqwe <vasyaqwe>
2023-11-22 00:21:51 +0200

    feat: add clerk user webhook

---
+++ src/app/api/webhook/user/route.ts
@@ -0,0 +1,71 @@
+import { db } from "@/server/db"
+import { type IncomingHttpHeaders } from "http"
+import { headers } from "next/headers"
+import { NextResponse } from "next/server"
+import { Webhook, type WebhookRequiredHeaders } from "svix"
+
+const webhookSecret = process.env.WEBHOOK_SECRET ?? ""
+
+async function handler(request: Request) {
+   const payload = await request.json()
+   const headersList = headers()
+   const heads = {
+      "svix-id": headersList.get("svix-id"),
+      "svix-timestamp": headersList.get("svix-timestamp"),
+      "svix-signature": headersList.get("svix-signature"),
+   }
+   const wh = new Webhook(webhookSecret)
+   let evt: Event | null = null
+
+   try {
+      evt = wh.verify(
+         JSON.stringify(payload),
+         heads as IncomingHttpHeaders & WebhookRequiredHeaders
+      ) as Event
+   } catch (err) {
+      console.error((err as Error).message)
+      return NextResponse.json({}, { status: 400 })
+   }
+
+   const eventType: EventType = evt.type
+   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+   const { id, ...attributes } = evt.data as any
+   if (eventType === "user.created" || eventType === "user.updated") {
+      await db.user.upsert({
+         where: { externalId: id as string },
+         create: {
+            externalId: id as string,
+            firstName: attributes.first_name as string,
+            lastName: attributes.last_name as string,
+            email: attributes?.email_addresses?.[0]?.email_address as string,
+            imageUrl: attributes.profile_image_url,
+            createdAt: new Date(attributes.created_at),
+            updatedAt: new Date(attributes.updated_at),
+         },
+         update: {
+            firstName: attributes.first_name as string,
+            lastName: attributes.last_name as string,
+            email: attributes?.email_addresses?.[0]?.email_address as string,
+            imageUrl: attributes.profile_image_url,
+         },
+      })
+   } else if (eventType === "user.deleted") {
+      await db.user.delete({
+         where: { externalId: id as string },
+      })
+   }
+
+   return new NextResponse("OK")
+}
+
+type EventType = "user.created" | "user.updated" | "user.deleted" | "*"
+
+type Event = {
+   data: Record<string, string | number>
+   object: "event"
+   type: EventType
+}
+
+export const GET = handler
+export const POST = handler
+export const PUT = handler
