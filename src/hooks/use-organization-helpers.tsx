8a2497814e1fa27e316384ee87dda281547cc79d
vasyaqwe <vasyaqwe>
2023-12-18 11:41:41 +0200

    fix: trying to fix error that only happens on vercel deployment

--- src/hooks/use-organization-helpers.tsx
+++ src/hooks/use-organization-helpers.tsx
@@ -1,10 +1,19 @@
+import { useEffect, useState } from "react"
+
 export function useOrganizationHelpers() {
-   const lastVisitedOrganizationId =
-      typeof window !== "undefined" && typeof localStorage !== "undefined"
-         ? JSON.parse(
-              localStorage.getItem("last-visited-organization-id") ?? ""
-           )
-         : ""
+   const [lastVisitedOrganizationId, setLastVisitedOrganizationId] =
+      useState("")
+
+   useEffect(() => {
+      const lastVisitedOrganizationId =
+         typeof window !== "undefined" && typeof localStorage !== "undefined"
+            ? JSON.parse(
+                 localStorage.getItem("last-visited-organization-id") ?? ""
+              )
+            : ""
+
+      setLastVisitedOrganizationId(lastVisitedOrganizationId)
+   }, [])
 
    return { lastVisitedOrganizationId }
 }
