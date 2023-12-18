e0751b2b510b5a2dc6894d22c6571a92a3ad8110
vasyaqwe <vasyaqwe>
2023-12-18 11:59:05 +0200

    fix: trying to fix error that only happens on vercel deployment

--- src/hooks/use-organization-helpers.tsx
+++ src/hooks/use-organization-helpers.tsx
@@ -1,10 +1,12 @@
 export function useOrganizationHelpers() {
+   let lastVisitedOrganizationId = ""
+
    if (typeof window !== "undefined" && typeof localStorage !== "undefined") {
       const storedValue = localStorage.getItem("last-visited-organization-id")
       const parsedValue = storedValue ? JSON.parse(storedValue) : ""
 
-      return { lastVisitedOrganizationId: parsedValue }
+      lastVisitedOrganizationId = parsedValue
    }
 
-   return ""
+   return { lastVisitedOrganizationId }
 }
