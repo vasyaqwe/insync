b496182142c47606d5a04a5cee7e15fc22f3cf67
vasyaqwe <vasyaqwe>
2023-12-29 15:12:42 +0200

    feat: use loading file for board page

---
+++ src/app/[locale]/(root)/dashboard/board/[boardId]/loading.tsx
@@ -0,0 +1,67 @@
+import { Skeleton } from "@/components/ui/skeleton"
+import { KanbanSquare } from "lucide-react"
+
+export default function Page() {
+   return (
+      <div className="grid h-full grid-cols-full-width-split-screen py-10 lg:py-12">
+         <div className="col-start-2 col-end-5 md:pl-[var(--container-padding-inline)]">
+            <h1 className="mt-2 flex items-center text-3xl font-medium">
+               <KanbanSquare
+                  className="mr-2"
+                  size={26}
+               />
+               <Skeleton className="h-5 w-40 rounded-lg" />
+            </h1>
+            <div className="flex h-full items-start gap-4 overflow-x-auto pr-4">
+               <div className="mt-6 flex items-start gap-4">
+                  <Skeleton className="w-[18rem] space-y-2 border bg-card p-3">
+                     <div className="flex items-center justify-between">
+                        <Skeleton className="h-5 w-36 rounded-lg" />
+                        <Skeleton className="h-9 w-9 rounded-lg" />
+                     </div>
+                     <Skeleton className="h-[46px] w-full rounded-lg" />
+                     <Skeleton className="h-[46px] w-full rounded-lg" />
+                     <Skeleton className="h-[46px] w-full rounded-lg" />
+                     <Skeleton className="h-[150px] w-full rounded-lg" />
+                  </Skeleton>
+                  <Skeleton className="w-[18rem] space-y-2 border bg-card p-3">
+                     <div className="flex items-center justify-between">
+                        <Skeleton className="h-5 w-36 rounded-lg" />
+                        <Skeleton className="h-9 w-9 rounded-lg" />
+                     </div>
+                     <Skeleton className="h-[46px] w-full rounded-lg" />
+                     <Skeleton className="h-[150px] w-full rounded-lg" />
+                     <Skeleton className="h-[46px] w-full rounded-lg" />
+                     <Skeleton className="h-[46px] w-full rounded-lg" />
+                     <Skeleton className="h-[46px] w-full rounded-lg" />
+                     <Skeleton className="h-[46px] w-full rounded-lg" />
+                     <Skeleton className="h-[150px] w-full rounded-lg" />
+                     <Skeleton className="h-[46px] w-full rounded-lg" />
+                  </Skeleton>
+                  <Skeleton className="w-[18rem] space-y-2 border bg-card p-3">
+                     <div className="flex items-center justify-between">
+                        <Skeleton className="h-5 w-36 rounded-lg" />
+                        <Skeleton className="h-9 w-9 rounded-lg" />
+                     </div>
+                     <Skeleton className="h-[46px] w-full rounded-lg" />
+                     <Skeleton className="h-[46px] w-full rounded-lg" />
+                     <Skeleton className="h-[150px] w-full rounded-lg" />
+                     <Skeleton className="h-[46px] w-full rounded-lg" />
+                     <Skeleton className="h-[46px] w-full rounded-lg" />
+                     <Skeleton className="h-[46px] w-full rounded-lg" />
+                  </Skeleton>
+                  <Skeleton className="w-[18rem] space-y-2 border bg-card p-3">
+                     <div className="flex items-center justify-between">
+                        <Skeleton className="h-5 w-36 rounded-lg" />
+                        <Skeleton className="h-9 w-9 rounded-lg" />
+                     </div>
+                     <Skeleton className="h-[46px] w-full rounded-lg" />
+                     <Skeleton className="h-[46px] w-full rounded-lg" />
+                     <Skeleton className="h-[46px] w-full rounded-lg" />
+                  </Skeleton>
+               </div>
+            </div>
+         </div>
+      </div>
+   )
+}
--- src/app/[locale]/(root)/dashboard/board/[boardId]/page.tsx
+++ src/app/[locale]/(root)/dashboard/board/[boardId]/page.tsx
@@ -1,16 +1,13 @@
 import { CreateList } from "@/components/forms/create-list"
 import { ListsWrapper } from "@/components/list"
-import { Skeleton } from "@/components/ui/skeleton"
 import { metadataConfig } from "@/config"
 import { pick } from "@/lib/utils"
 import { db } from "@/server/db"
-import { api } from "@/trpc/server"
 import { currentUser } from "@clerk/nextjs"
-import { LayoutIcon } from "lucide-react"
+import { KanbanSquare } from "lucide-react"
 import { NextIntlClientProvider } from "next-intl"
 import { getMessages } from "next-intl/server"
 import { notFound } from "next/navigation"
-import { Suspense } from "react"
 
 type Params = {
    params: { boardId: string }
@@ -39,6 +36,11 @@ export default async function Page({ params: { boardId } }: Params) {
       select: {
          id: true,
          name: true,
+         lists: {
+            include: {
+               cards: true,
+            },
+         },
          organization: {
             select: {
                members: true,
@@ -56,9 +58,9 @@ export default async function Page({ params: { boardId } }: Params) {
       <div className="grid h-full grid-cols-full-width-split-screen py-10 lg:py-12">
          <div className="col-start-2 col-end-5 md:pl-[var(--container-padding-inline)]">
             <h1 className="text-3xl font-medium">
-               <LayoutIcon
+               <KanbanSquare
                   className="mr-2 inline"
-                  size={30}
+                  size={26}
                />
                {board.name}
             </h1>
@@ -71,76 +73,14 @@ export default async function Page({ params: { boardId } }: Params) {
                      "editor",
                   ])}
                >
-                  <Suspense fallback={<ListsSkeleton />}>
-                     <Lists boardId={boardId} />
-                     <CreateList boardId={boardId} />
-                  </Suspense>
+                  <ListsWrapper
+                     className="mt-6"
+                     lists={board.lists}
+                  />
+                  <CreateList boardId={boardId} />
                </NextIntlClientProvider>
             </div>
          </div>
-      </div>
-   )
-}
-
-async function Lists({ boardId }: { boardId: string }) {
-   const lists = await api.list.getAll.query({ boardId })
-
-   return (
-      <ListsWrapper
-         className="mt-6"
-         lists={lists}
-      />
-   )
-}
-
-function ListsSkeleton() {
-   return (
-      <div className="mt-6 flex items-start gap-4">
-         <Skeleton className="w-[18rem] space-y-2 border bg-card p-3">
-            <div className="flex items-center justify-between">
-               <Skeleton className="h-5 w-36 rounded-lg" />
-               <Skeleton className="h-9 w-9 rounded-lg" />
-            </div>
-            <Skeleton className="h-[46px] w-full rounded-lg" />
-            <Skeleton className="h-[46px] w-full rounded-lg" />
-            <Skeleton className="h-[46px] w-full rounded-lg" />
-            <Skeleton className="h-[150px] w-full rounded-lg" />
-         </Skeleton>
-         <Skeleton className="w-[18rem] space-y-2 border bg-card p-3">
-            <div className="flex items-center justify-between">
-               <Skeleton className="h-5 w-36 rounded-lg" />
-               <Skeleton className="h-9 w-9 rounded-lg" />
-            </div>
-            <Skeleton className="h-[46px] w-full rounded-lg" />
-            <Skeleton className="h-[150px] w-full rounded-lg" />
-            <Skeleton className="h-[46px] w-full rounded-lg" />
-            <Skeleton className="h-[46px] w-full rounded-lg" />
-            <Skeleton className="h-[46px] w-full rounded-lg" />
-            <Skeleton className="h-[46px] w-full rounded-lg" />
-            <Skeleton className="h-[150px] w-full rounded-lg" />
-            <Skeleton className="h-[46px] w-full rounded-lg" />
-         </Skeleton>
-         <Skeleton className="w-[18rem] space-y-2 border bg-card p-3">
-            <div className="flex items-center justify-between">
-               <Skeleton className="h-5 w-36 rounded-lg" />
-               <Skeleton className="h-9 w-9 rounded-lg" />
-            </div>
-            <Skeleton className="h-[46px] w-full rounded-lg" />
-            <Skeleton className="h-[46px] w-full rounded-lg" />
-            <Skeleton className="h-[150px] w-full rounded-lg" />
-            <Skeleton className="h-[46px] w-full rounded-lg" />
-            <Skeleton className="h-[46px] w-full rounded-lg" />
-            <Skeleton className="h-[46px] w-full rounded-lg" />
-         </Skeleton>
-         <Skeleton className="w-[18rem] space-y-2 border bg-card p-3">
-            <div className="flex items-center justify-between">
-               <Skeleton className="h-5 w-36 rounded-lg" />
-               <Skeleton className="h-9 w-9 rounded-lg" />
-            </div>
-            <Skeleton className="h-[46px] w-full rounded-lg" />
-            <Skeleton className="h-[46px] w-full rounded-lg" />
-            <Skeleton className="h-[46px] w-full rounded-lg" />
-         </Skeleton>
       </div>
    )
 }
